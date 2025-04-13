f896955ce22cb2a8c5e46cf48bf989d823301609
joaopbessa1998 <joaopbessa1998@gmail.com>
2025-04-13 21:17:05 +0100

    feat: add backend main entry point

---
+++ backend/src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.routes';
+import hotelRoutes from './routes/hotel.routes';
+
+dotenv.config(); // lê variáveis de ambiente do ficheiro .env
+
+// conectar ao mongoDB
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    console.log('MongoDB conectado com sucesso!');
+  })
+  .catch((error) => {
+    console.error('Erro de conexão ao MongoDB:', error);
+  });
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// rota de teste
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('Pong');
+});
+
+// rota de autenticação
+app.use('/auth', authRoutes);
+app.use('/hotels', hotelRoutes);
+
+// iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor a rodar na porta ${PORT}`);
+});
